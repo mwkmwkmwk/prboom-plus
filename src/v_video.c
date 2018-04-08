2303a8a365171f24b0496d56dd45d9e75711dc69
Marcin Kościelnicki <koriakin@0x04.net>
2018-04-08 09:42:06 +0200

    fixes

--- src/v_video.c
+++ src/v_video.c
@@ -669,6 +669,7 @@ static void V_DrawMemPatch(int x, int y, int scrn, const rpatch_t *patch,
           dcvars.edgeslope &= ~RDRAW_EDGESLOPE_TOP_MASK;
         }
 
+        dcvars.texture_fd = patch->doomdev_fd;
         dcvars.texture_base = column->pixels;
         dcvars.source = column->pixels + post->topdelta + yoffset;
         dcvars.prevsource = prevcolumn ? prevcolumn->pixels + post->topdelta + yoffset: dcvars.source;
